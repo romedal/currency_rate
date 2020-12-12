784cc2e60167975aa1d4405b5526041ec26b8d8f
Sergii Brodeskur <romedal7@gmail.com>
2020-12-12 02:39:32 +0200

    Getting currency rates for period

--- app/src/main/java/com/example/romedal/MainActivity.java
+++ app/src/main/java/com/example/romedal/MainActivity.java
@@ -16,9 +16,15 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "romedalTAG";
-    public static final String URL = "https://api.exchangeratesapi.io/latest?symbols=USD";
+    private static final String TAG = "r_TAG";
+    private static final String URL_CUR_SYMBOL = "https://api.exchangeratesapi.io/latest?symbols=USD";
+    private static final String URL_CUR_PERIOD = "https://api.exchangeratesapi.io/history?start_at=2020-12-01&end_at=2020-12-11&symbols=USD&&base=EUR";
 
     StringRequest stringRequest;
     RequestQueue queue;
@@ -33,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         final TextView textView = (TextView) findViewById(R.id.txtv);
 
         queue = Volley.newRequestQueue(this);
-        stringRequest = new StringRequest(Request.Method.GET, URL,
+        stringRequest = new StringRequest(Request.Method.GET, URL_CUR_SYMBOL,
                 new Response.Listener<String>() {
                     float usd10000eur = 0;
                     @Override
@@ -49,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                             Log.d(TAG, "after multiplay " + usd10000eur);
 
                         } catch (JSONException e) {
-                            Log.e(TAG, "exception romedal");
+                            Log.e(TAG, "exception is caught by romedal");
                             e.printStackTrace();
                         }
 
@@ -66,6 +72,65 @@ public class MainActivity extends AppCompatActivity {
         queue.add(stringRequest);
 
     }
+
+    public void getPeriodReqExchange(android.view.View d) {
+
+        final TextView textView = (TextView) findViewById(R.id.txtv);
+        queue = Volley.newRequestQueue(this);
+
+        stringRequest = new StringRequest(Request.Method.GET, URL_CUR_PERIOD,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        String result = "";
+                        Log.d(TAG, "respone =>" + response);
+                        try {
+                            JSONObject ResponseObject = new JSONObject(response.trim());
+                            JSONObject rates = ResponseObject.getJSONObject("rates");
+                            Iterator<String> keys = rates.keys();
+                            Map treeMap = new TreeMap<Integer, Integer>();
+                            for (int i = 0; i < rates.names().length(); i++) {
+                                Log.v(TAG, "" + Integer.parseInt(rates.names().getString(i).substring(8)));
+                                treeMap.put(Integer.parseInt(rates.names().getString(i).substring(8)), i);
+                            }
+
+                            Set keyss = treeMap.keySet();
+                            for (Iterator i = keyss.iterator(); i.hasNext(); ) {
+                                Integer key = (Integer) i.next();
+                                Integer value = (Integer) treeMap.get(key);
+                                Log.v(TAG, key + " =>>>>>>> " + value);
+                                JSONObject ResponseObjectusd = new JSONObject(rates.get(rates.names().getString(value)).toString());
+
+                                result += String.format("10K EUR is: %d $ within the date %s \r\n", (int) calculate10K_eur(ResponseObjectusd.get("USD").toString()), rates.names().getString(value));
+
+                            }
+
+                        } catch (JSONException e) {
+                            Log.e(TAG, "exception is caught by romedal");
+                            e.printStackTrace();
+                        }
+
+                        textView.setText(result);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        stringRequest.setTag(TAG);
+        queue.add(stringRequest);
+    }
+
+    private float calculate10K_eur(String usd) {
+        float usd10000eur = Float.parseFloat(usd);
+        Log.d(TAG, "converted => " + usd10000eur);
+        usd10000eur *= 10000;
+        Log.d(TAG, "after multiplay => " + usd10000eur);
+        return usd10000eur;
+    }
+
     @Override
     protected void onStop () {
         super.onStop();
